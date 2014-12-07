429634800a4a9e69a8583cfaf835cbe217ed3933
samrooney <sam_rooney@asl.org>
2014-12-07 13:02:07 +0000

    finished

--- Lab10.java
+++ Lab10.java
@@ -1,23 +1,45 @@
 /**
  * An algorithm helps an Athlete solve a maze, perhaps by keeping one hand against a wall.
  * 
- * @author <...>
+ * @author <Sam. >
  * @version <...>
  *
  */
 
 import edu.fcps.karel2.Display; 
+import javax.swing.JOptionPane;
 
 public class Lab10 {
 	 
 	 public static void main(String[] args) {
-		 //TODO Open the map 'maze1' or 'maze2' or 'maze3';
-		 //TODO Set the display to size 10 by 10
+		 String map = JOptionPane.showInputDialog("Which map?");
+		 Display.openWorld("maps/"+map+".map");
+		 Display.setSize(10, 10);
 		 Athlete athena = new Athlete(1, 1, Display.NORTH, Display.INFINITY);
 		 clearMaze(athena);
 	 }
 	 
 	 public static void clearMaze(Athlete arg) {
-		 //TODO Write an algorithm that directs arg to the beeper at the end of the maze
-	 }
+	 while (!arg.nextToABeeper()){
+       while (!arg.frontIsClear()&&!arg.leftIsClear()&&!arg.rightIsClear()){
+       arg.turnAround();
+       arg.move();
+       }
+       while (arg.frontIsClear()&&!arg.leftIsClear()){
+       arg.move();
+       }
+       while (arg.frontIsClear()&&arg.leftIsClear()&&!arg.rightIsClear()){
+       arg.turnLeft();
+       arg.move();
+       }
+       while (!arg.frontIsClear()&&!arg.leftIsClear()&&arg.rightIsClear()){
+       arg.turnRight();
+       arg.move();
+       }
+       while (!arg.frontIsClear()&&arg.leftIsClear()){
+       arg.turnLeft();
+       arg.move();
+       }
+    }
+ }
  }
